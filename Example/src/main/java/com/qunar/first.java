c9c64e2b08b699064fdefade5e8047005c911299
曹 <247595158@qq.com>
2017-05-02 20:12:32 +0800

    hhsadshds

---
+++ Example/src/main/java/com/qunar/first.java
@@ -0,0 +1,10 @@
+package com.qunar;
+
+/**
+ * Created by 曹 on 2017/5/2.
+ */
+public class first {
+    public static void main(String[] args){
+        System.out.println("hellel");
+    }
+}
---
+++ Example/src/main/java/controller/MainController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+/**
+ * Created by 曹 on 2017/5/2.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+    //测试guava
+    public static void main(String arg[]){
+        Map<String, Integer> left = ImmutableMap.of("a", 1, "b", 2, "c", 3);
+
+        Map<String, Integer>  right= ImmutableMap.of("a", 1, "b", 2, "c", 3);
+
+        MapDifference<String, Integer> diff = Maps.difference(left, right);
+
+        diff.entriesInCommon(); // {"b" => 2}
+        diff.entriesInCommon(); // {"b" => 2}
+        System.out.println(diff.entriesOnlyOnLeft());
+
+        System.out.println( diff.entriesOnlyOnRight());
+
+    }
+}
+
